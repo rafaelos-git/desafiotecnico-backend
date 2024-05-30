769d46bfeed025f7a575be90e1aa4c2f10565085
Rafael Oliveira Silva <rafael.oliveiratdm@outlook.com>
2024-05-30 12:43:35 -0300

    initial config

---
+++ src/main/java/br/com/sicredi/desafio_rafael/DesafioRafaelApplication.java
@@ -0,0 +1,13 @@
+package br.com.sicredi.desafio_rafael;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioRafaelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioRafaelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/sicredi/desafio_rafael/DesafioRafaelApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sicredi.desafio_rafael;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioRafaelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
