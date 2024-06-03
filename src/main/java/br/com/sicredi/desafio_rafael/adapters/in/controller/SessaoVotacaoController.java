96ade6eeb916768f1f1ff25e610d19d89765d515
Rafael Oliveira Silva <rafael.oliveiratdm@outlook.com>
2024-06-03 02:12:15 -0300

    feat: versionamento de api na url

--- src/main/java/br/com/sicredi/desafio_rafael/adapters/in/controller/SessaoVotacaoController.java
+++ src/main/java/br/com/sicredi/desafio_rafael/adapters/in/controller/SessaoVotacaoController.java
@@ -11,7 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/sessoes")
+@RequestMapping("/api/v1/sessoes")
 public class SessaoVotacaoController {
     @Autowired
     private OpenSessaoVotacaoInputPort openSessaoVotacaoInputPort;
